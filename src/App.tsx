1e2f3b3683936b288953cb884b84e76e9ecbf7ea
abr-ya <gamer05@list.ru>
2022-09-23 21:42:26 +0300

    init: Start project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./app.scss";
+
+const App = () => (
+  <div className="app">
+    <h1>Hello, React 2022-09!</h1>
+    <h2>ENV test: {process.env.TEST}</h2>
+  </div>
+);
+
+export default App;
