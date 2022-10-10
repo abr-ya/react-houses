2ff32e63557e2ba98272d2c0fb72fdbe979821f3
abr-ya <gamer05@list.ru>
2022-10-10 08:48:10 +0300

    feat: Add title and link to Main Slider

--- src/components/SliderSwiper6/SliderSwiper6.styled.tsx
+++ src/components/SliderSwiper6/SliderSwiper6.styled.tsx
@@ -1,9 +1,42 @@
 import styled from "styled-components/macro";
 
-export const SliderItem = styled.div<{ height: number; bg: string }>`
+export const SliderItem = styled.div<{ height: number; bg: string; isLink?: boolean }>`
   position: relative;
   width: 100%;
   height: ${({ height }) => height}px;
   background: url(${({ bg }) => bg}) center no-repeat;
   background-size: "cover";
+  cursor: ${({ isLink }) => (isLink ? "pointer" : "all-scroll")};
+`;
+
+export const SlideTitle = styled.p`
+  color: #ffffff;
+  position: absolute;
+  top: 70px;
+  left: 0;
+  font-weight: 600;
+  max-width: 90%;
+  font-size: 1.25rem;
+  background-color: rgba(0, 0, 0, 0.8);
+  padding: 0.5rem;
+
+  @media (min-width: 1024px) {
+    font-size: 1.75rem;
+  }
+`;
+
+export const SlidePrice = styled.p`
+  color: #000000;
+  position: absolute;
+  top: 120px;
+  left: 11px;
+  font-weight: 600;
+  max-width: 90%;
+  background-color: #ffffff;
+  padding: 0.25rem 0.5rem;
+  border-radius: 1rem;
+
+  @media (min-width: 1024px) {
+    font-size: 1.25rem;
+  }
 `;
--- src/components/SliderSwiper6/SliderSwiper6Data.tsx
+++ src/components/SliderSwiper6/SliderSwiper6Data.tsx
@@ -1,8 +1,9 @@
-import { IHouse } from "interfaces";
+import { useNavigate } from "react-router-dom";
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
-import { SliderItem } from "./SliderSwiper6.styled";
+import { SlidePrice, SliderItem, SlideTitle } from "./SliderSwiper6.styled";
+import { IHouse } from "interfaces";
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
@@ -11,18 +12,27 @@ interface ISliderSwiper6Data {
   height: number;
 }
 
-const SliderSwiper6Data = ({ dataArray, height }: ISliderSwiper6Data) => (
-  <div>
-    <Swiper slidesPerView={1} pagination={{ clickable: true }}>
-      {dataArray.map(({ id, data }) =>
-        !data?.imageUrls ? null : (
-          <SwiperSlide key={id}>
-            <SliderItem height={height} bg={data.imageUrls[0]} />
-          </SwiperSlide>
-        ),
-      )}
-    </Swiper>
-  </div>
-);
+const SliderSwiper6Data = ({ dataArray, height }: ISliderSwiper6Data) => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {dataArray.map(({ id, data }) =>
+          !data?.imageUrls ? null : (
+            <SwiperSlide key={id} onClick={() => navigate(`/category/${data.type}/${id}`)}>
+              <SliderItem height={height} bg={data.imageUrls[0]} isLink>
+                <SlideTitle>{data.name}</SlideTitle>
+                <SlidePrice>
+                  {data.discountedPrice ?? data.regularPrice} {data.type === "rent" && "/ month"}
+                </SlidePrice>
+              </SliderItem>
+            </SwiperSlide>
+          ),
+        )}
+      </Swiper>
+    </div>
+  );
+};
 
 export default SliderSwiper6Data;
